13e022837fb353e59f8aaab35d24e0b36f282c12
Leonardo Marinho <leowalkg6@gmail.com>
2024-12-09 19:26:42 -0300

    add serial version

--- src/exceptions/connException.java
+++ src/exceptions/connException.java
@@ -2,6 +2,11 @@ package exceptions;
 
 public class connException extends DbException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public connException(String message, Throwable cause ){
 		super(message,cause);
 	}
